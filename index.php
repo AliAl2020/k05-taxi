eea3648d169410fbee63352ca40af05cd67e092e
ali albaalbaki <albaalbakiali@outlook.com>
2025-03-19 22:30:50 +0100

    add new logo

--- index.php
+++ index.php
@@ -10,115 +10,58 @@
 
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
-    <style>
-        body, html {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-            color: #fff;
-            background-color: #000;
-            overflow-x: hidden;
-    
-        }
-body {
-    padding-top: 70px; /* Damit der Inhalt nicht hinter dem fixen Navbar verschwindet */
+<link rel="stylesheet" href="css/style.css">
+<link rel="icon" type="image/png" href="images/logo.png">
+<link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+<link rel="shortcut icon" href="/images/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+<link rel="manifest" href="/images/site.webmanifest" />
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "url": "https://taxi-schleissheim.de",
+  "logo": "https://taxi-schleissheim.de/images/logo.png"
 }
-
-
-        .navbar, footer {
-            background-color: #222;
-        }
-
-        .nav-link, .navbar-brand {
-            color: #ffc107 !important;
-        }
-
-        h2, h3 {
-            color: #ffc107;
-        }
-
-        .carousel-item img {
-            height: 100vh;
-            object-fit: cover;
-            filter: brightness(50%);
-        }
-
-        #start {
-            position: relative;
-        }
-
-        .overlay-content {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 10;
-            text-align: center;
-        }
-
-        .overlay-content h1 {
-            font-size: 50px;
-            font-weight: bold;
-        }
-
-        .btn-call {
-            background-color: #ffc107;
-            color: #000;
-            padding: 10px 20px;
-            font-size: 20px;
-            text-decoration: none;
-            border-radius: 5px;
-        }
-
-        .navbar {
-    padding-top: 5px;
-    padding-bottom: 5px;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 1000;
-}
-
-.icon-circle {
-            font-size: 50px;
-            color: #ffc107;
-            margin-bottom: 15px;
-        }
-
-        .section-padding {
-            padding: 60px 0;
-        }
-
-        footer {
-            padding: 20px 0;
-            text-align: center;
-            color: #bbb;
-        }
-    </style>
+</script>
 </head>
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Logo" style="height:100px; vertical-align:middle;"></a>
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            <img src="images/logo.png" alt="Logo" style="height: 60px; vertical-align: middle;">
+        </a>
+        
+        <!-- Navbar Toggler for Mobile -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#start">Startseite</a></li>
-                <li class="nav-item"><a class="nav-link" href="#leistung">Leistung</a></li>
-                <li class="nav-item"><a class="nav-link" href="#preise">Preise</a></li>
-                <li class="nav-item"><a class="nav-link" href="#impressum">Impressum</a></li>
-                <li class="nav-item"><a class="nav-link" href="#kontakt">Kontakt</a></li>
+        
+        <!-- Navbar Links -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-warning fw-bold" href="#start">Startseite</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning fw-bold" href="#leistung">Leistung</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning fw-bold" href="#preise">Preise</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning fw-bold" href="#impressum">Impressum</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning fw-bold" href="#kontakt">Kontakt</a>
+                </li>
             </ul>
         </div>
     </div>
 </nav>
-
 <!-- Slider Startseite -->
 <section id="start">
     <div id="carouselExample" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
@@ -136,7 +79,7 @@ body {
     </div>
     <div class="overlay-content">
         <h1>TAXI ASM Unterschleißheim</h1>
-        <a href="tel:089 3119 8242" class="btn-call">Bitte rufen Sie +49 (0)89 3119 8242 an</a>
+        <a href="tel:089 3119 8242" class="btn-call">Bitte rufen Sie uns an</a>
     </div>
 </section>
 <!-- Leistungen (nur Taxi) -->
@@ -203,7 +146,7 @@ body {
 <!-- Kontakt -->
 <section id="kontakt" class="section-padding">
     <div class="container">
-        <h2 class="text-center mb-5">Kontakt & Buchung</h2>
+        <h2 class="text-center mb-5">Kontakt </h2>
         <div class="row">
             <!-- Linke Spalte: Kontaktinformationen -->
             <div class="col-md-6">
@@ -226,13 +169,16 @@ body {
                 </div>
             </div>
 
-            <!-- Rechte Spalte: Google Maps -->
             <div class="col-md-6">
-                <div class="card bg-dark text-white p-2">
-                    <h4 class="mb-3"><i class="fa fa-map-marker-alt"></i> Unser Standort</h4>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10619.532253546873!2d11.563910885763832!3d48.28586550103142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479e71d1d3233431%3A0x77df63b13525465b!2sOhmstra%C3%9Fe%202%2C%2085716%20Unterschlei%C3%9Fheim!5e0!3m2!1sen!2sde!4v1742341431737!5m2!1sen!2sde" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+    <div class="card bg-dark text-white p-3">
+        <h4 class="mb-3"><i class="fa fa-map-marker-alt"></i> Unser Standort</h4>
+        <div class="google-map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10619.532253546873!2d11.563910885763832!3d48.28586550103142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479e71d1d3233431%3A0x77df63b13525465b!2sOhmstra%C3%9Fe%202%2C%2085716%20Unterschlei%C3%9Fheim!5e0!3m2!1sen!2sde!4v1742341431737!5m2!1sen!2sde"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
+    </div>
+</div>
+
     </div>
 </section>
 
